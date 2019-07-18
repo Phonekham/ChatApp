45e255af8aa4ef71c1cb098741e58bec8bc63474
Phonekham <phonekham.dev@gmail.com>
2019-07-18 15:17:55 +0700

    8. Sharing Your Location

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,7 +4,21 @@ socket.on("message", message => {
   console.log(message);
 });
 
-// document.querySelector("#message-form").addEventListener("submit", e => {
-//   e.preventDefault();
-//   socket.emit("message");
-// });
+document.querySelector("#message-form").addEventListener("submit", e => {
+  e.preventDefault();
+  const message = e.target.elements.message.value;
+  socket.emit("sendMessage", message);
+});
+
+document.querySelector("#send-location").addEventListener("click", () => {
+  if (!navigator.geolocation) {
+    return alert("navigator.geolocation is not supported by your browser");
+  }
+  navigator.geolocation.getCurrentPosition(position => {
+    // console.log(position);
+    socket.emit("sendLocation", {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  });
+});
--- src/index.js
+++ src/index.js
@@ -18,8 +18,11 @@ io.on("connection", socket => {
   socket.emit("message", "Welcome");
   socket.broadcast.emit("message", "A new user has joined");
 
-  socket.on("sendMessage", message => {
-    io.emit("message", message);
+  socket.on("sendLocation", coords => {
+    io.emit(
+      "message",
+      `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
+    );
   });
 
   socket.on("disconnect", () => {
