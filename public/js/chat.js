72535e24b4a8dcd503205fcf25d92f22cf1d915d
Phonekham <phonekham.dev@gmail.com>
2019-07-18 16:34:01 +0700

    10. Form and Button States

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,13 +1,25 @@
 const socket = io();
 
+// elements
+const $messageFrom = document.querySelector("#message-form");
+const $messageFromInput = $messageFrom.querySelector("input");
+const $messageFromButton = $messageFrom.querySelector("button");
+const $sendLocationButton = document.querySelector("#send-location");
+
 socket.on("message", message => {
   console.log(message);
 });
 
-document.querySelector("#message-form").addEventListener("submit", e => {
+$messageFrom.addEventListener("submit", e => {
   e.preventDefault();
+
+  $messageFromButton.setAttribute("disabled", "disabled");
+
   const message = e.target.elements.message.value;
   socket.emit("sendMessage", message, error => {
+    $messageFromButton.removeAttribute("disabled");
+    $messageFromInput.value = "";
+    $messageFromInput.focus();
     if (error) {
       return console.log(error);
     }
@@ -15,10 +27,11 @@ document.querySelector("#message-form").addEventListener("submit", e => {
   });
 });
 
-document.querySelector("#send-location").addEventListener("click", () => {
+$sendLocationButton.addEventListener("click", () => {
   if (!navigator.geolocation) {
     return alert("navigator.geolocation is not supported by your browser");
   }
+  $sendLocationButton.setAttribute("disabled", "disabled");
   navigator.geolocation.getCurrentPosition(position => {
     // console.log(position);
     socket.emit(
@@ -28,6 +41,7 @@ document.querySelector("#send-location").addEventListener("click", () => {
         longitude: position.coords.longitude
       },
       () => {
+        $sendLocationButton.removeAttribute("disabled");
         console.log("Location shared");
       }
     );
