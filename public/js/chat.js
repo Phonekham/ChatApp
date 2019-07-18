2db50616c0059826b86cb182b0768160d7048936
Phonekham <phonekham.dev@gmail.com>
2019-07-18 16:11:41 +0700

    9. Event Acknowledgements

--- public/js/chat.js
+++ public/js/chat.js
@@ -7,7 +7,12 @@ socket.on("message", message => {
 document.querySelector("#message-form").addEventListener("submit", e => {
   e.preventDefault();
   const message = e.target.elements.message.value;
-  socket.emit("sendMessage", message);
+  socket.emit("sendMessage", message, error => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("the message was delivered");
+  });
 });
 
 document.querySelector("#send-location").addEventListener("click", () => {
@@ -16,9 +21,15 @@ document.querySelector("#send-location").addEventListener("click", () => {
   }
   navigator.geolocation.getCurrentPosition(position => {
     // console.log(position);
-    socket.emit("sendLocation", {
-      latitude: position.coords.latitude,
-      longitude: position.coords.longitude
-    });
+    socket.emit(
+      "sendLocation",
+      {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+      },
+      () => {
+        console.log("Location shared");
+      }
+    );
   });
 });
