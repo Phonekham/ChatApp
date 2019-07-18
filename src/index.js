a6f23cecda20cac3b58042f3a0b4b3c70378c04e
Phonekham <phonekham.dev@gmail.com>
2019-07-18 14:41:13 +0700

    7. Broadcasting Events

--- src/index.js
+++ src/index.js
@@ -15,11 +15,15 @@ let count = 0;
 
 io.on("connection", socket => {
   console.log("New WebSocket connection");
-  socket.emit("countUpdated", count);
-  socket.on("increment", () => {
-    count++;
-    // socket.emit("countUpdated", count);
-    io.emit("countUpdated", count);
+  socket.emit("message", "Welcome");
+  socket.broadcast.emit("message", "A new user has joined");
+
+  socket.on("sendMessage", message => {
+    io.emit("message", message);
+  });
+
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has left");
   });
 });
 
